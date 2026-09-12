46cfcdac7a6b7a0a5a64292605ee212b70019784
DavidCardenoITM <davidcardeno1114031@correo.itm.edu.co>
2026-09-12 17:29:14 -0500

    first commit whith a structure

---
+++ src/main/java/com/itm/edu/co/ProyectoPP_2026/ProyectoPp2026Application.java
@@ -0,0 +1,13 @@
+package com.itm.edu.co.ProyectoPP_2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPp2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPp2026Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/itm/edu/co/ProyectoPP_2026/ProyectoPp2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itm.edu.co.ProyectoPP_2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoPp2026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
